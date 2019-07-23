4a39057c5b7317befce27ee2ffba0620dc70ecb3
Maxwell Yinger <max@Maxwells-iMac-Pro.local>
2019-07-22 22:19:28 -0600

    Updates

---
+++ precache-manifest.abac0189cf6397a26f73e60412e79b65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f071574e077db2bea9935a90bb45e5d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a44d0ca18671851210df",
+    "url": "/static/css/3.38b236b9.chunk.css"
+  },
+  {
+    "revision": "346464400ebbefa462b9",
+    "url": "/static/css/main.fc4ed7b7.chunk.css"
+  },
+  {
+    "revision": "baa3fb71363df8e53cf5",
+    "url": "/static/js/2.1835c9c3.chunk.js"
+  },
+  {
+    "revision": "a44d0ca18671851210df",
+    "url": "/static/js/3.1d5e3604.chunk.js"
+  },
+  {
+    "revision": "346464400ebbefa462b9",
+    "url": "/static/js/main.92a9c46c.chunk.js"
+  },
+  {
+    "revision": "9794953e1c0ff3a80b59",
+    "url": "/static/js/runtime~main.7dfdce5a.js"
+  },
+  {
+    "revision": "f0fd1f4cacb7e8570e8d6007e588988f",
+    "url": "/static/media/logo.f0fd1f4c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/muzzlemooninn/precache-manifest.2aff6ad4251925fd071418ee969a26ae.js"
+  "/precache-manifest.abac0189cf6397a26f73e60412e79b65.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/muzzlemooninn/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
